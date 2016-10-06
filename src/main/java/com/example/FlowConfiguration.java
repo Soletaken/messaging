5023859fc6582d0ccfc3ad479d44fa8122143810
Marcin Pawlak <marcin.pawlak@impaqgroup.com>
2016-10-06 21:29:50 +0200

    break

--- src/main/java/com/example/FlowConfiguration.java
+++ src/main/java/com/example/FlowConfiguration.java
@@ -10,26 +10,19 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.amqp.channel.PointToPointSubscribableAmqpChannel;
 import org.springframework.integration.annotation.IntegrationComponentScan;
-import org.springframework.integration.channel.interceptor.WireTap;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.channel.DirectChannelSpec;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.retry.RetryOperations;
-import org.springframework.retry.interceptor.RetryOperationsInterceptor;
-import org.springframework.retry.support.RetryTemplate;
 
 import java.util.Map;
 import java.util.Optional;
 
 import static java.util.concurrent.Executors.newCachedThreadPool;
-import static org.springframework.integration.dsl.channel.MessageChannels.direct;
 
 
 @Configuration
@@ -41,52 +34,67 @@ public class FlowConfiguration {
     private ConnectionFactory connectionFactory;
 
     @Bean
-    MessageChannel someChannel(){
+    MessageChannel someChannel() {
 //        final DirectChannelSpec direct = direct(Utilis.INBOUND_CHANNEL);
 //        return direct.get();
         Optional<QueueArguments> arguments = Optional.of(new QueueArguments());
-        String queueName="some.name.for.queue";
+        String queueName = "some.name.for.queue";
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.setTaskExecutor(newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat(queueName+".%d").build()));
+        container.setTaskExecutor(newCachedThreadPool(new ThreadFactoryBuilder().setNameFormat(queueName + ".%d").build()));
         container.setConcurrentConsumers(5);
         container.setMaxConcurrentConsumers(100);
         PointToPointSubscribableAmqpChannel channel =
                 new PointToPointSubscribableAmqpChannel(Utilis.INBOUND_CHANNEL, container, template) {
-            @Override
-            protected String obtainQueueName(AmqpAdmin admin, String channelName) {
-                arguments.ifPresent( (a)-> {
-                    admin.declareQueue(new Queue(queueName, true, false, false, a.asMap()));
-                });
-                return super.obtainQueueName(admin, channelName);
-            }
-        };
+                    @Override
+                    protected String obtainQueueName(AmqpAdmin admin, String channelName) {
+                        arguments.ifPresent((a) -> {
+                            admin.declareQueue(new Queue(queueName, true, false, false, a.asMap()));
+                        });
+                        return super.obtainQueueName(admin, channelName);
+                    }
+                };
         channel.setQueueName(queueName);
 
-        
+
         return channel;
     }
 
     @Bean
-    public IntegrationFlow testFlow(HandlerInter handlerInter){
+    public IntegrationFlow testFlow(HandlerInter handlerInter) {
         return flow -> flow
                 .channel(Utilis.INBOUND_CHANNEL)
-                .transform(message ->{
+                .transform(message -> {
                     System.out.println(message);
+                    return message;
+                })
+                .channel(Utilis.TEST_CHANNEL);
+    }
+
+    @Bean
+    public IntegrationFlow testFlow2(HandlerInter handlerInter) {
+        return flow -> flow
+                .channel(Utilis.TEST_CHANNEL)
+                .transform(message -> {
+                    System.out.println("message in another channel: " + message);
                     return message;
                 })
                 .handle(handlerInter)
-                .transform(message ->{
-                    System.out.println(message);
+                .routeToRecipients(r-> r.recipientFlow(
+                        msg -> true, f -> f.channel(Utilis.TEST_CHANNEL2)))
+                .channel(Utilis.TEST_CHANNEL2)
+                .transform(message -> {
+                    System.out.println("message after handler: " + message);
                     return message;
-                })
-                .bridge(null);
+                });
+
+//                .bridge(null);
     }
 
 
     @Data
-    private static class QueueArguments{
+    private static class QueueArguments {
 
         private int xMaxLength = Integer.MAX_VALUE;
 
--- src/main/java/com/example/util/Utilis.java
+++ src/main/java/com/example/util/Utilis.java
@@ -2,4 +2,6 @@ package com.example.util;
 
 public class Utilis {
     public static final String INBOUND_CHANNEL = "my.new.channel";
+    public static final String TEST_CHANNEL = "test.channel";
+    public static final String TEST_CHANNEL2 = "test2.channel2";
 }
